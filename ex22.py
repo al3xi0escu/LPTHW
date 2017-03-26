31fe09e326dc66f53676ea79e7e0f0ad32884b24
al3xi0escu <alexionescu@seoulchristian.org>
2017-03-26 09:46:59 +0900

    Did ex 22

---
+++ ex22.py
@@ -0,0 +1,30 @@
+# print = a variable that writes something in the code
+# # = this symbol let's you comment
+# + = plus; adds
+# - = minus; subtracts
+# / = slash
+# * = asterisk; multiplies
+# % = percent; modulus operator
+# < = less-than
+# > = greater-than
+# <= = less-than-equal
+# >= = greater-than-equal
+# "" = string
+# '' = string
+# () = parenthesis; it makes the elements unique
+# , = used to separate each item in a parameter
+# \ = encodes difficult to type characters into a string
+# raw_input = takes some kind of of input from a person, changes it,print out something to show it changed
+# import = how you add features to your script
+# argv = holds the arguments you pass to your Python script
+# second, first, second, third = rather than holding all the arguments, it gets assigned to four variables you can work with
+# close = closes the file
+# read = reads the content of the file
+# readline = reads just one line of a text file
+# truncate = empties the file
+# write('stuff') = writes 'stuff' to the file
+# *args = it's alot like argv but for functions
+# def = defines a function
+# = = names variables
+# return = to set variables to be a value from a function
+# 
